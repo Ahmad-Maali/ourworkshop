28ef15ea4e5dd7509d2cd26a9979dfc98ffdf521
user <user@DESKTOP-N31RR2H>
2024-11-04 00:27:26 +0200

    hello

--- Workshop/src/application/mainController.java
+++ Workshop/src/application/mainController.java
@@ -1,8 +1,16 @@
 package application;
+
+
+
+
+
 import java.io.IOException;
+
+import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,9 +19,10 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-	public class Controller {
+	public class mainController {
 
 	    @FXML
 	    private TextField idField;
@@ -31,28 +40,47 @@ import javafx.stage.Stage;
 	   
 
 	    //
+	    
+	    
 		Parent root;
-		
 		Stage stage;
-
+		FXMLLoader Loader;
 		
-		  @FXML private void handleLogin(ActionEvent e)
-		  { 
-			  
-			  String id = idField.getText(); 
-			  String password = passwordField.getText();
-			  
-			  
+		
+		  @FXML private void handleLogin(ActionEvent e) throws IOException {
 		  
-		  // Implement your login logic here
-		  System.out.println("ID: " + id + ", Password: " + password); 
-		  }
+		  String id = idField.getText(); String password = passwordField.getText();
+		  
+		  
+		  // فحص للباسورد
+		  
+		  
+		  
+		  Loader = new FXMLLoader(getClass().getResource("Admin.fxml")); Parent root =
+		  Loader.load();
+		  
+		  // Create a new Group and add the loaded root node to it 
+	//	  Group groupRoot =  new Group();
+	//	  groupRoot.getChildren().add(root);
+		  
+		  
+		  // Set up the new Scene with the Group as root
+		  stage = (Stage) ((Node)e.getSource()).getScene().getWindow();
+		  Scene scene = new Scene(root);
+		  stage.setScene(scene); stage.show();
+		  
+
+		  }	
+		
+		
+		
+		
 		  
 		  @FXML 
 		  private void handleRegister(ActionEvent e) throws IOException { // Implement registration logic here 
 		  System.out.println("Registration clicked"); 
 		  
-				  FXMLLoader Loader = new FXMLLoader (getClass().getResource("registration.fxml"));
+				  Loader = new FXMLLoader (getClass().getResource("registration.fxml"));
 				  root= Loader.load();
 				  stage =(Stage)((Node)e.getSource()).getScene().getWindow();
 				  Scene scene = new Scene(root); 
